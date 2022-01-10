fd5cc2810441f6988ff2b9d432d81993016e8af8
CO18344 <42909454+CO18344@users.noreply.github.com>
2022-01-10 17:09:27 +0530

    Delete Delivery directory

--- Delivery/static/js/load.js
+++
@@ -1,9 +0,0 @@
-var navCustom = document.querySelector('.navCustom');
-
-var cross = document.querySelector('.form-main img');
-cross.addEventListener('click',dismiss);
-
-function dismiss(){
-    navCustom.classList.add('active');
-    //navCustom.remove();
-} 
