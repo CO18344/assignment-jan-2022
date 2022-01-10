5b2332f9732de3c3fef034a86541dd0be5036d37
CO18344 <satviksingh28@gmail.com>
2022-01-10 17:28:53 +0530

    removed sensitive information

--- Week1/Delivery/Delivery/settings.py
+++ Week1/Delivery/Delivery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&)cq4=k*^(&w@d05$hbaw1*!z3ugo^$$690y1wyx#3l-z()w*k'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -106,12 +106,7 @@ AUTH_USER_MODEL = 'customauth.MyUser'
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ssmailtest200@gmail.com'
-EMAIL_HOST_PASSWORD = 'Laser123@'
+
 
 LANGUAGE_CODE = 'en-us'
 
